bc110e38ea5c4f8c3b6e0ebb0ed5d800d38f6c14
Nico Schneider <nico.tcap@gmail.com>
2014-10-15 19:21:06 +0200

    cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,28 +12,32 @@ var gulp = require('gulp'),
 
 // Set some vars
 
-var themepath = './files/theme/'
+var themePath = './files/theme/';
+var bowerPath = './files/theme/assets/';
 
 // The GULP Tasks
+
+// Define default and special tasks
+gulp.task('default', ['css_minify', 'copy_fonts']);
+gulp.task('dev', ['watch']);
 
 // Watch the css direcotry for changes and trigger the live reload
 // For livereload see https://github.com/vohof/gulp-livereload
 gulp.task('watch', ['css_main'], function () {
-    gulp.watch(themepath + 'less/main.less', ['css_main']);
-    gulp.watch(themepath + 'less/partials/*.less', ['css_main']);
+    gulp.watch(themePath + 'less/**/*.less', ['css_main']);
     livereload.listen();
-    gulp.watch(themepath + 'css/**').on('change', livereload.changed);
+    gulp.watch(themePath + 'css/**').on('change', livereload.changed);
 });
 
 // Compile our css and push it to the public webfolder
 gulp.task('css_main', function () {
-    gulp.src(themepath + 'less/main.less')
+    gulp.src(themePath + 'less/main.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(prefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(header("/* This file is generated — do not edit by hand! */\n"))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(themepath + 'css/'));
+        .pipe(gulp.dest(themePath + 'css/'));
 });
 
 // Minify the css
@@ -42,19 +46,15 @@ gulp.task('css_minify', ['css_main'], function() {
         .pipe(concat('all.css'))
         .pipe(minifyCss())
         .pipe(header("/* This file is generated — do not edit by hand! */\n"))
-    .pipe(gulp.dest(themepath + 'css/'));
+    .pipe(gulp.dest(themePath + 'css/'));
 });
 
 // We need the bootstrap fonts in public, lets copy
 gulp.task('copy_fonts', function () {
     var combined = combine(
-        gulp.src(['./bower_components/bootstrap/fonts/**/*.{ttf,woff,eof,svg}']),
-        gulp.src(['./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}']),
-        gulp.dest(themepath + 'fonts')
+        gulp.src([bowerPath + 'bootstrap/fonts/**/*.{ttf,woff,eof,svg}']),
+        gulp.src([bowerPath + 'font-awesome/fonts/**/*.{ttf,woff,eof,svg}']),
+        gulp.dest(themePath + 'fonts')
     );
     combined.on('error', console.error.bind(console));
 });
-
-// Define default and special tasks
-gulp.task('default', ['css_minify', 'copy_fonts']);
-gulp.task('dev', ['watch']);
