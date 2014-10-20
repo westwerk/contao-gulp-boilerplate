be8d49efd29869b98c27869694ca3715f29508e9
Nico Schneider <nico.tcap@gmail.com>
2014-10-20 11:30:23 +0200

    add main script file

---
+++ files/theme/js/main.js
@@ -0,0 +1,18 @@
+requirejs.config({
+    baseUrl: '/files/theme/assets/',
+    paths: {
+        jquery: 'jquery/dist/jquery.min',
+        bootstrap: 'bootstrap/dist/js/bootstrap.min'
+    },
+    shim: {
+        bootstrap: ['jquery']
+    }
+});
+
+require(['jquery'], function($) {
+
+    $(document).ready(function() {
+        console.log('JS asset setup working!');
+    });
+
+});
