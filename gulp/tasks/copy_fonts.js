d7f64d27a64f0cfdda4b0efb59011f6802eb2cae
Nico Schneider <nico.tcap@gmail.com>
2015-03-05 11:05:03 +0100

    Also copy woff2 font format

--- gulp/tasks/copy_fonts.js
+++ gulp/tasks/copy_fonts.js
@@ -6,12 +6,12 @@ gulp.task('copy_fonts', function () {
     return gulp
         .src([
             // bootstrap glyphicons
-            config.bowerPath + 'bootstrap/fonts/**/*.{ttf,woff,eot,svg}',
+            config.bowerPath + 'bootstrap/fonts/**/*.{ttf,woff,woff2,eot,svg}',
             // fontawesome, if you have it
-            config.bowerPath + 'fontawesome/fonts/**/*.{ttf,woff,eot,svg}',
+            config.bowerPath + 'fontawesome/fonts/**/*.{ttf,woff,woff2,eot,svg}',
             // alternative spelling
-            config.bowerPath + 'font-awesome/fonts/**/*.{ttf,woff,eot,svg}',
-            config.bowerPath + 'Font-awesome/fonts/**/*.{ttf,woff,eot,svg}'
+            config.bowerPath + 'font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}',
+            config.bowerPath + 'Font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}'
         ])
         .pipe(gulp.dest(config.themePath + 'fonts'));
 });
